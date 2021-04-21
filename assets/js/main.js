254f6410cfa26a17d768b0acc291c665656f1da5
AndreiBurbulia <Andreiburbulia@hotmail.it>
2021-04-21 15:39:05 +0200

    Inserimento file Js e prova

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+
+
+//Writeln
+document.writeln("Sono stato inserito con JS!");
+//alert
+alert("Se vuoi visualizzare la pagina clicca OK!")
+//getelementById
+document.getElementById("titolo_prova_js").innerHTML = "JS ha deciso che e sono Future Played"
